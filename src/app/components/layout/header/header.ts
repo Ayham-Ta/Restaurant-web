15b7e0f1c411f53f958980f5d83ab1d63d2402c5
Ayham-Ta <ayhamaltaki004@gmail.com>
2025-08-17 20:53:50 +0300

    #2

--- src/app/components/layout/header/header.ts
+++ src/app/components/layout/header/header.ts
@@ -6,32 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
 })
-export class Header implements AfterViewInit {
-  @ViewChild('carousel') carouselRef!: ElementRef;
-  currentIndex = 0;
-  
-  // Sample menu items
-  menuItems = [
-    { name: 'Home', icon: 'fa-home' },
-    { name: 'Menu', icon: 'fa-utensils' },
-    { name: 'About', icon: 'fa-info-circle' },
-    { name: 'Book Table', icon: 'fa-calendar-alt' }
-  ];
-
-  activeItem = this.menuItems[0].name;
-
-  ngAfterViewInit() {
-    this.initCarousel();
-  }
-
-  private initCarousel() {
-    const carousel = this.carouselRef.nativeElement;
-    // Carousel initialization code here
-  }
-
-  setActive(item: string) {
-    this.activeItem = item;
-  }
+export class Header{
   orderonline(){
     console.log('111111');
     
